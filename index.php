e778f79f99a325f6d4887da8b3b891bcfd09d988
Krisnen <153392045+Krisnen@users.noreply.github.com>
2024-03-04 00:25:04 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<!-- saved from url=(0025)http://localhost/project/ -->
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        
+        <meta http-equiv="X-UA-COmpatible" content="IE edge">
+        <meta name="viewport" content="width-device-width, initial-scale-1.0">
+
+        <link rel="stylesheet" href="Design.css">
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <script src="https://unpkg.com/typed.js@2.0.15/dist/typed.umd.js"></script>
+
+    
+    </head>
+    <body>
+        <header class="header">
+          <a href="#" class="logo">Portfolio</a>
+          <nav class="navbar">
+          <a href="#">Home</a> 
+          <a href="#about">About</a> 
+          <a href="#Skills">Skill</a> 
+          <a href="#">Portfolio</a> 
+          <a href="#contact">Contact</a> 
+          </nav>
+        </header>
+        <section class="home">
+            <div class="home-content">
+                <h3>HI THERE,</h3>
+                <h1>Kris Here</h1>
+                <h3>Im <span class="text"></span></h3>
+                <p>I'm looking forward to working with you </p><br>
+            <a href="#about" class="btn-box">More About me</a>
+            </div>
+            <span class="home-imgHover"></span>
+        </section>
+        <section class="about" id="about">
+            <div class="about-img">
+                <img src="me10-removebg-preview.png">
+
+            </div>
+            <div class="about-text">
+                <h2>About <span>Me</span></h2>
+                <h4>An aspiring Data Analyst</h4>
+                <p>With an IT background through extra curriculars since 
+                    my middle school days and a yearning for physical systems 
+                    I have always found joy in predicting experimental results 
+                    through analysis of huge junks of data, that is exaclty where 
+                    my programming knowledge comes in handy espcially Python.
+                    I have an understanding of time series analysis which can be employed 
+                    to interpolate out absent values and alsomake further predictions through
+                    various indicators. I also enjoy working with differnt search algorithms
+                    aimed at AI and training and testing of models using simulation
+                    modules like SIMULINK in MATLAB or other packages such as scipy, numpy,
+                    pandas, etc. in case of python. 
+                </p>
+                <a href ="#Skills" class="btn-box">More About Me</a>
+            </div>
+        </section>
+        <section>
+            <div class="services" id="services">
+            <div class="container">
+                <h1 class="sub-title">My<span> Internships</span></h1>
+                <div class="service-list">
+                    <div>
+                    <i class='bx bx-code-alt' style='color:#2ee7eb'  ></i>
+                        <h2>Web Development</h2>
+                        <p>Deployment of front-end websites using Wordpress
+                        and also subsequent modification of themes and plugins suited to the 
+                        the needs of a client. Server maintainance and file migration through cPanel manually or 
+                        using varius plugins as WP Migrate.    
+                        </p>
+                        <a href="#Skills" class="read">learn more</a>
+                    </div>
+                    <div>
+                    <i class='bx bx-pulse' style='color:#2aeaef'  ></i>
+                        <h2>Embedded Systems</h2>
+                        <p>Development of scripts for differnt embedded systems related to security.
+                            I have also worked with IOT devices such as GSM and Bluetooth modules. My projects include
+                            fire management system (mostly based on DHT sensors and GSM for remote communication) and 
+                            Specific motion sensitive alarms using PIR sensors 
+
+                        </p>
+                        <a href="#Skills" class="read">learn more</a>
+                    </div>
+                    
+            </div>
+            </div>
+        </section>
+        <h1 class="sub-title">My <span>Skills</span></h1>
+        <section>
+            <div class="container1" id="Skills">
+                <h1 class="heading1">Technical Skills</h1>
+                <div class="Technical-bars">
+                    <div class="bar">
+                        <div class="info"><i class='bx bxl-python' style='color:#067bb2'  ></i><br>
+                        <span>Python</span>
+                        </div>
+                    </div>
+                    <div class="progress-line python">
+                        <span></span>
+                    </div>
+                    <br>
+                    <div class="bar"><i class='bx bxl-java' style='color:#067bb2'></i><br>
+                        <div class="info">
+                        <span>JAVA</span>
+                        </div>
+                    </div>
+                    <div class="progress-line java">
+                        <span></span>
+                    </div>
+                    <br>
+                    <div class="bar"><i class='bx bx-code' style='color:#0cf806'></i><br>
+                        <div class="info">
+                        <span>C</span>
+                        </div>
+                    </div>
+                    <div class="progress-line c">
+                        <span></span>
+                    </div>
+                    <br>
+                    <div class="bar"><i class='bx bxl-html5' style='color:#b24006'></i><br>
+                        <div class="info">
+                        <span>HTML</span>
+                        </div>
+                    </div>
+                    <div class="progress-line html">
+                        <span></span>
+                    </div>
+                    <br>
+                    <div class="bar"><i class='bx bxl-css3' style='color:#067bb2'></i><br>
+                        <div class="info">
+                        <span>CSS</span>
+                        
+                        </div>
+                    </div>
+                    <br>
+                    <div class="progress-line css">
+                        <span></span>
+                    </div>
+                    <div class="bar"><i class='bx bxl-bootstrap' style='color:#3e06b2'></i><br>
+                        <div class="info">
+                        <span>Bootstarp</span>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="progress-line bootstrap">
+                        <span></span>
+                    </div>
+                    <br>
+                    <div class="bar"><i class='bx bxl-wordpress' style='color:#1c08ce'></i><br>
+                        <div class="info">
+                        <span>Wordpress</span>
+                        </div>
+                    </div>
+                    <div class="progress-line wordpress">
+                        <span></span>
+                    </div>
+                    <br>
+                    <div class="bar"><i class='bx bx-chip' style='color:#f8e706'></i><br>
+                        <div class="info">
+                        <span>Arduino</span>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="progress-line arduino">
+                        <span></span>
+                    </div>
+                    <br>
+                    <div class="bar"><i class='bx bx-calculator' style='color:#067bb2'></i><br>
+                        <div class="info">
+                        <span>MATLAB</span>
+                        </div>
+                    </div>
+                    <div class="progress-line MATLAB">
+                        <span></span>
+                    </div>
+                    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+                </div>
+            </div>
+
+            <div class="container1">
+                <h1 class="heading1">Topics Studied</h1>
+                <div class="radial-bars">
+                  <div class="radial-bar">
+                    <svg x="0px" y="0px" viewbox="0 0 200 200">
+                        <circle class="progress-bar" cx="100" cy="100" r="80"></circle>
+                        <circle class="path path-1" cx="100" cy="100" r="80"></circle>                   
+                    </svg>
+                    <div class="percentage">90%</div>
+                    <div class="text">Classical Mechanics</div>
+                  </div>
+                 <div class="radial-bar">
+                    <svg x="0px" y="0px" viewbox="0 0 200 200">
+                        <circle class="progress-bar" cx="100" cy="100" r="80"></circle>
+                        <circle class="path path-2" cx="100" cy="100" r="80"></circle>                   
+                    </svg>
+                    <div class="percentage">70%</div>
+                    <div class="text">Electrodynamics</div>
+                 </div>
+                 <div class="radial-bar">
+                    <svg x="0px" y="0px" viewbox="0 0 200 200">
+                        <circle class="progress-bar" cx="100" cy="100" r="80"></circle>
+                        <circle class="path path-3" cx="100" cy="100" r="80"></circle>                   
+                    </svg>
+                    <div class="percentage">75%</div>
+                    <div class="text">Special Relativity</div>
+                 </div>
+                 <div class="radial-bar">
+                    <svg x="0px" y="0px" viewbox="0 0 200 200">
+                        <circle class="progress-bar" cx="100" cy="100" r="80"></circle>
+                        <circle class="path path-4" cx="100" cy="100" r="80"></circle>                   
+                    </svg>
+                    <div class="percentage">80%</div>
+                    <div class="text">Quantum Mechanics</div>
+                 </div>
+                
+                </div>
+            </div>
+        </section>
+        <section class="contact" id="contact">
+            <div class="contact-text">
+                <h2>Contact <span>Me</span></h2>
+                <h4>Let's work together</h4>
+                <p>You can reach out to me through the contacts listed below:</p>
+                <div class="contact-list">
+                    <li><i class='bx bxl-gmail' style='color:#08f1b0'  ></i>kmajumdar450@gmail.com</li>
+                    <li><i class='bx bxl-whatsapp' style='color:#07f3b2'  ></i>+49 015236736452</li>
+
+                </div>
+
+            </div>
+            
+
+            </div>
+
+        </section>
+        <script src="main.js"></script>
+</body></html>
---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["Studying Physics", "a Tech Enthusiast", "also into Music"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
